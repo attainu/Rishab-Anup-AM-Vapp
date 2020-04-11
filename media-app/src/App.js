aea01b51d5247dd11ff3d6549ef96f49aa5fdb1b
anup kumar <anupkumar.599@gmail.com>
2020-04-11 09:48:42 +0530

    frontend

---
+++ media-app/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
+import {SideBar} from './containers/SideBar/SideBar';
+// import {Home} from './containers/Home/Home';
+// import './Home.scss';
+
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <HeaderNav/>
+        <SideBar/>
+        {/* <Home/> */}
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ media-app/src/containers/SideBar/SideBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SideBarItem} from './SideBarItem/SideBarItem';
+import {Menu} from 'semantic-ui-react';
+import './SideBar.scss';
+
+export class SideBar extends React.Component {
+  render() {
+    
+        return (
+          <Menu borderless vertical stackable fixed='left' className='side-nav'>
+            <SideBarItem highlight={true}  label='Home' icon='home'/>
+            <SideBarItem label='Movies and Shows' icon='film'/>
+            
+          </Menu>
+        );
+      }
+  }
